38c4dc6ab650fd9ea42b4f4c637db2f6ba312f61
VestaCute <anton799190@yandex.ru>
2020-12-10 11:53:49 +0300

    add forms Validation

---
+++ Validators.swift
@@ -0,0 +1,32 @@
+//
+//  Validators.swift
+//  wios
+//
+//  Created by Sasha Kondratjeva on 10.12.2020.
+//
+
+import Foundation
+
+class Validators {
+    static func isField (email: String?, password: String?, confirmPassword: String?) -> Bool {
+        guard let password = password,
+              let confirmPassword = confirmPassword,
+              let email = email,
+              password != "",
+              confirmPassword != "",
+              email != "" else {
+            return false
+        }
+        return true
+    }
+    
+    static func isSimpleEmail(_ email: String) -> Bool {
+        let emailRegEx = "^.+@.+\\..{2,}$"
+        return check(text: email, regEx: emailRegEx)
+    }
+    
+    private static func check(text: String, regEx: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: text)
+    }
+}
